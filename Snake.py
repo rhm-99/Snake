5f14a929edf0e91bf708aca51791036189eb4ab2
Rhiya <rhiyamehta99@yahoo.co.uk>
2020-05-23 22:42:15 +0100

    commit

--- Snake.py
+++ Snake.py
@@ -1,7 +1,8 @@
 # Learnt from https://techwithtim.net/tutorials/game-development-with-python/snake-pygame/tutorial-1/
 # (may be slightly edited)
 import random
-import snake
+import tkinter as tk
+from tkinter import messagebox
 import pygame
 
 
@@ -9,7 +10,7 @@ class Cube(object):
     rows = 20
     w = 400
 
-    def __init__(self, start, dirnx=1, dirny=0, color=(0, 0, 0)):
+    def __init__(self, start, dirnx=1, dirny=0, color=(0, 255, 0)):
         self.pos = start
         self.dirnx = 1
         self.dirny = 0
@@ -25,15 +26,15 @@ class Cube(object):
         i = self.pos[0]
         j = self.pos[1]
 
-        pygame.draw.rect(surface, self.color, (i*dis+1, dis-2, dis-2))
+        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
 
         if eyes:
-            centre = dis//2
+            centre = dis // 2
             radius = 3
-            circleMiddle = (i*dis+dis-radius, j*dis+8)
-            circleMiddle2 = (i*dis + dis-radius*2, j*dis+8)
-            pygame.draw.circle(surface, (255, 255, 255), circleMiddle, radius)
-            pygame.draw.circle(surface, (255, 255, 255), circleMiddle2, radius)
+            circleMiddle = (i * dis + dis - radius, j * dis + 8)
+            circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
 
 
 class Snake(object):
@@ -57,49 +58,61 @@ class Snake(object):
                     self.dirnx = -1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
                 elif key[pygame.K_RIGHT]:
                     self.dirnx = 1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
                 elif key[pygame.K_DOWN]:
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
                 elif key[pygame.K_UP]:
                     self.dirnx = 0
                     self.dirny = -1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
         for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
                 turn = self.turns[p]
                 c.move(turn[0], turn[1])
-                if i == len(self.body)-1:
+                if i == len(self.body) - 1:
                     self.turns.pop(p)
             else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1:
+                if c.dirnx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows - 1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
                     c.pos = (0, c.pos[1])
                 elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
                     c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
-                else: c.move(c.dirnx, c.dirny)
+                elif c.dirny == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows - 1)
+                else:
+                    c.move(c.dirnx, c.dirny)
 
     def reset(self, pos):
-        pass
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
     def addCube(self):
         tail = self.body[-1]
         dx, dy = tail.dirnx, tail.dirny
 
-        if dx == 1 and dy ==0:
-            self.body.append(cube((tail.pos[0]-1, tail.pos[1])))
-        elif dx == -1 and dy ==0:
-            self.body.append(cube((tail.pos[0]+1, tail.pos[1])))
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0] + 1, tail.pos[1])))
         elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0], tail.pos[1]-1)))
+            self.body.append(cube((tail.pos[0], tail.pos[1] - 1)))
         elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0], tail.pos[1]+1)))
+            self.body.append(cube((tail.pos[0], tail.pos[1] + 1)))
 
         self.body[-1].dirnx = dx
         self.body[-1].dirn = dy
@@ -138,7 +151,7 @@ def randomSnack(rows, item):
     while True:
         x = random.randrange(rows)
         y = random.randrange(rows)
-        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
             continue
         else:
             break
@@ -147,7 +160,14 @@ def randomSnack(rows, item):
 
 
 def message_box(subject, content):
-    pass
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def main():
@@ -174,9 +194,14 @@ def main():
 
     if s.body[0] == snake.pos:
         s.addCube()
-        snack = cube(randomSnack(rows, s), color = (0, 255, 0))
+        snack = cube(randomSnack(rows, s), color=(0, 255, 0))
 
-    for x in range()
+    for x in range(len(s.body))
+        if s.body[x].pos in list(map(lamda z:z.pos, s.body[x + 1:])):
+        print('Score: ', len(s.body))
+        message_box('Game Over!', 'Play again...')
+        s.reset((10, 10))
+        break
 
 
 main()
